8c78d8707fec031631805061601ca21d96d50fc1
Jussi Kukkonen <jkukkonen@google.com>
2024-01-03 15:12:01 +0200

    DEBUG: try minimal test
    Signed-off-by: Jussi Kukkonen <jkukkonen@google.com>

--- test/test_simple.py
+++ test/test_simple.py
@@ -1,23 +1,12 @@
-from test.conftest import _MakeMaterials
-
 import pytest  # type: ignore
 
 from .client import SigstoreClient
 
 
-@pytest.mark.signing
-def test_simple(client: SigstoreClient, make_materials: _MakeMaterials) -> None:
+
+
+def test_simple(client: SigstoreClient) -> None:
     """
-    A simple test that signs and verifies an artifact for a given Sigstore
-    client.
+    A simple test that does nothing
     """
-
-    input_path, materials = make_materials("a.txt")
-    assert not materials.exists()
-
-    # Sign the artifact.
-    client.sign(materials, input_path)
-    assert materials.exists()
-
-    # Verify the artifact signature.
-    client.verify(materials, input_path)
+    pass
